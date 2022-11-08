f337c1f7e9c7f3ccf60ca16d62a15bdf17401d0f
Bjorkor <barkdawg11@gmail.com>
2022-11-08 16:26:06 -0600

    local men in your area

--- localMan.py
+++ localMan.py
@@ -53,5 +53,5 @@ def scrape():
     #megaZord = pd.merge(left=localMansPants, right=orderMansPants, how='left', on='increment_id')
     #print(megaZord)
     #print(localMansPants.dtypes)
-    print(localMansPants['increment_id'].astype(str).astype(int, errors='ignore').dtypes)
+    print(localMansPants['increment_id'].astype(str).astype(int, errors='ignore').convert_dtypes().dtypes)
 scrape()
