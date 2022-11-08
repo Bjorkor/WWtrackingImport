47fb75b63b70d257b77f2e7ce7befc04a6071fa2
Bjorkor <barkdawg11@gmail.com>
2022-11-08 16:32:50 -0600

    local men in your area

--- localMan.py
+++ localMan.py
@@ -47,7 +47,9 @@ def scrape():
     cnxn.close()
 
     localMansPants.rename(columns={"TransId": "traverse_id", "TrackingNum": "tracking", "cf_External Trans Id": "increment_id"}, inplace=True)
-    localMansPants = localMansPants.convert_dtypes().astype('str', errors='ignore')
+    localMansPants['increment_id'] = localMansPants['increment_id'].convert_dtypes().astype('int', errors='ignore')
+    localMansPants['tracking'] = localMansPants['tracking'].convert_dtypes().astype('str', errors='ignore')
+    localMansPants['traverse_id'] = localMansPants['traverse_id'].convert_dtypes().astype('str', errors='ignore')
     orderMansPants['increment_id'] = orderMansPants['increment_id'].convert_dtypes().astype('int', errors='ignore').convert_dtypes()
     #localMansPants2 = localMansPants.apply(lambda x: x.astype('object'))
     #orderMansPants2 = orderMansPants.apply(lambda x: x.astype('object'))
