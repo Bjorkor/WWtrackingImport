464a09961c608d164c317b9134986abe7a38f13e
Bjorkor <barkdawg11@gmail.com>
2022-11-09 15:36:57 -0600

    local men in your area

--- scratch.py
+++ scratch.py
@@ -1,33 +1,51 @@
-
 import pandas as pd
 import pymongo
+import requests
 from dotenv import load_dotenv
 import os
 import datetime
 from orderMan import order
+import json
+from requests.sessions import Session
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread, local
+import time
 
-#local = pd.read_csv('local.csv')
-#order = pd.read_csv('order.csv')
+
+def get_session() -> Session:
+    if not hasattr(thread_local, 'session'):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+
+thread_local = local()
+now = datetime.datetime.utcnow()
+target = now - datetime.timedelta(hours=2)
+# .strftime("%Y-%m-%d %H:%M:%S")
+
 load_dotenv()
-dbaddr = os.getenv('DBADDR')
-client = pymongo.MongoClient(dbaddr)
-db = client["wwmongo"]
-orders = db["orders"]
-def work(local, order):
-    local['tracking'] = local[local['tracking'].astype('string')]
-    print('local')
-    print(local.dtypes)
-    print(local)
-    print('order')
-    print(order.dtypes)
-    print(order)
-print('i am doing thingsssssss')
-nums = [999999, 999999, 999999, 300574]
-list = []
-for x in nums:
-    print(f'checking for {x}')
-    if orders.find_one({'entity_id': x}):
-        print('yes')
-    else:
-        print('noooo')
+token = os.getenv('BEARER')
+headers = {'Authorization': f'Bearer {token}'}
+session = get_session()
+list = ['2000301013', '2000301012', '2000301011', '2000301010', '', '2000301009']
 
+for increment_id in list:
+    with session as session:
+        api_url = f'https://wwhardware.com/rest/default/V1/orders?searchCriteria[pageSize]=1&searchCriteria[filterGroups][0][filters][0][field]=increment_id&searchCriteria[filterGroups][0][filters][0][value]={increment_id}'
+
+        response = session.get(api_url, headers=headers)
+        # print(response.status_code)
+        # print(response.content)
+        if response.status_code == 200:
+            y = json.loads(response.content)
+            print(y)
+                # time.sleep(5)
+        if response.status_code == 400:
+            print(response.content)
+        if response.status_code != 200 and response.status_code != 400:
+            # print('FFFFFF')
+            while response.status_code != 200 and response.status_code != 400:
+                print('retrying...')
+                time.sleep(10)
+                response = session.get(api_url, headers=headers)
+                print(response.status_code)
