3b1f717bee5c16f2bd6b5f2a2499fa9eb77d06fe
Bjorkor <barkdawg11@gmail.com>
2022-11-09 13:39:15 -0600

    local men in your area

--- scratch.py
+++ scratch.py
@@ -22,7 +22,7 @@ def work(local, order):
     print(order)
 
 nums = [999999, 999999, 999999, 300487]
-for x in len(nums):
+for x in nums:
     if orders.find({ "entity_id": x }):
         print('we got em')
     else:
