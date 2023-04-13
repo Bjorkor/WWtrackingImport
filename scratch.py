868e31e47abdd32d191bd3b79c6f03f23f94d19d
Bjorkor <barkdawg11@gmail.com>
2023-04-13 14:24:18 -0500

    Update scratch.py

---
+++ scratch.py
@@ -0,0 +1,28 @@
+import sqlite3 as sl
+import requests
+from requests.sessions import Session
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread, local
+import time
+import datetime
+from dotenv import load_dotenv
+import os
+from orderMan import order
+import localMan
+import json
+import pymongo
+import traceback
+
+global client
+global db
+global orders
+import re
+
+load_dotenv()
+dbaddr = os.getenv('DBADDR')
+client = pymongo.MongoClient(dbaddr)
+db = client["wwmongo"]
+orders = db["orders"]
+
+for x in orders.find():
+    print(order(x))
