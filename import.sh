145a883a55412c8b4f95d98fa9e705274af51b90
Bjorkor <barkdawg11@gmail.com>
2023-04-14 08:49:44 -0500

    Create import.sh

---
+++ import.sh
@@ -0,0 +1,11 @@
+  GNU nano 4.8                                                                                                                                               run.sh
+#!/bin/bash
+
+# Author : Zara Ali
+# Copyright (c) Tutorialspoint.com
+# Script follows here:
+
+echo 'pulling source'
+activate="/home/ftp/WWtrackingImport/venv/bin/activate"
+source "$activate"
+python /home/ftp/WWtrackingImport/autoimport.py
