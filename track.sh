f14801b6c9ba27e5ed82eb275cd499cc2d8236e6
Bjorkor <barkdawg11@gmail.com>
2023-04-14 10:07:01 -0500

    asdf

--- track.sh
+++ track.sh
@@ -1,9 +1,4 @@
-GNU nano 4.8                                                                                                                                               run.sh
-#!/bin/bash
 
-# Author : Zara Ali
-# Copyright (c) Tutorialspoint.com
-# Script follows here:
 
 echo 'pulling source'
 activate="/home/ftp/WWtrackingImport/venv/bin/activate"
