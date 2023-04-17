af2ee73882345db0ad2309c8e2a4d79da7f67d08
Bjorkor <barkdawg11@gmail.com>
2023-04-17 08:29:09 -0500

    Update track.sh

--- track.sh
+++ track.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# send start tick to cronitor
+curl https://cronitor.link/p/e12853e7180849b99497cbf55f5b8859/kTj0MO?state=run
+
 # Set the path to your virtual environment directory
 VENV_DIR="/home/ftp/WWtrackingImport/venv"
 
@@ -11,6 +14,17 @@ PYTHON_SCRIPT="/home/ftp/WWtrackingImport/app.py"
 
 # Execute the Python script
 python "$PYTHON_SCRIPT"
+
+
+# check for success
+if [ $? == 0 ]
+then
+        # if success, report success to cronitor
+        curl https://cronitor.link/p/e12853e7180849b99497cbf55f5b8859/kTj0MO?state=complete
+else
+        # if fail, report failure to cronitor
+        curl https://cronitor.link/p/e12853e7180849b99497cbf55f5b8859/kTj0MO?state=fail
+fi
 
 # Deactivate the virtual environment
 deactivate
