5974f8b335b80203e53f83918a661322dd8bf5c4
Bjorkor <barkdawg11@gmail.com>
2023-05-03 12:31:09 -0500

    Update drop.py

--- drop.py
+++ drop.py
@@ -1,12 +1,16 @@
 from dotenv import load_dotenv
 import pymongo
 import os
+import datetime
 load_dotenv()
 dbaddr = os.getenv('DBADDR')
 client = pymongo.MongoClient(dbaddr)
 db = client["wwmongo"]
 orders = db["orders"]
-q = { 'isTracked': True }
+
+
+d = datetime.datetime(2023, 4, 28, 21, 46, 8, 597000)
+q = { "dateCreated": {"$lt": d}}
 x = orders.delete_many(q)
 
 print(x.deleted_count, " documents deleted.")
