508d0c45306a3d69d6e75385a114fc547825003d
Bjorkor <barkdawg11@gmail.com>
2023-05-03 12:29:57 -0500

    Update q.py

--- q.py
+++ q.py
@@ -1,11 +1,18 @@
 from dotenv import load_dotenv
 import pymongo
 import os
+import datetime
 load_dotenv()
 dbaddr = os.getenv('DBADDR')
 client = pymongo.MongoClient(dbaddr)
 db = client["wwmongo"]
 orders = db["orders"]
-myquery = { "isTracked": False }
+
+d = datetime.datetime(2023, 4, 28, 21, 46, 8, 597000)
+
+
+myquery = { "dateCreated": {"$lt": d}}
 for x in orders.find(myquery):
     print(x)
+
+
