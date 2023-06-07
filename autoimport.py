2384512dc809021cabf1a4001098d9df1cdeee61
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-06-07 14:57:37 -0500

    Update autoimport.py

--- autoimport.py
+++ autoimport.py
@@ -36,7 +36,7 @@ def recallLastOrder():
     try:
         with open('lastorder', 'w') as f:
             content = f.read()
-        return content
+        return int(content)
     except:
         return 0
 
