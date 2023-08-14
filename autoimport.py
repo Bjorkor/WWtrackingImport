4c5b1ce93084ef774c2aecc8449555a5a7477d63
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-08-14 10:50:23 -0500

    update email to alerts

--- autoimport.py
+++ autoimport.py
@@ -495,7 +495,7 @@ with session as session:
                 order = order.apply(lambda x: x[:30] if isinstance(x, str) else x)
                 order = order.str.strip()
                 order = order.str.replace(r'\s+', ' ').replace('"', '').replace("'", '').replace("`", '').replace(",", '')
-                
+
                 #add the series created above to the blank dataframe created at the beginning of the script
                 df = pd.concat([df, order.to_frame().T])
 
@@ -638,7 +638,7 @@ with session as session:
 
 #setup email message
 
-from_email = "sales@hdlusa.com"
+from_email = "alerts@hdlusa.com"
 from_password = os.getenv('EMAIL_CRED')
 to_emails = ["tbarker@hdlusa.com", "cstachowski@hdlusa.com", "BReichard@hdlusa.com", "sales@hdlusa.com"]
 
