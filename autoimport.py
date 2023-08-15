cf3419330e1b1699c3104a9a0b40884881ef2df9
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-08-15 15:22:37 -0500

    Update autoimport.py

--- autoimport.py
+++ autoimport.py
@@ -505,9 +505,9 @@ with session as session:
                 order = pd.Series(order_dict)
 
                 #handle fields too long and disallowed chars
-                order = order.apply(lambda x: x[:30] if isinstance(x, str) else x)
+                """order = order.apply(lambda x: x[:30] if isinstance(x, str) else x)
                 order = order.str.strip()
-                order = order.str.replace(r'\s+', ' ').replace('"', '').replace("'", '').replace("`", '')
+                order = order.str.replace(r'\s+', ' ').replace('"', '').replace("'", '').replace("`", '')"""
 
                 #add the series created above to the blank dataframe created at the beginning of the script
                 df = pd.concat([df, order.to_frame().T])
