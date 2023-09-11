ee825ad9b2ea0798ac155669460110de88bbf05d
Bjorkor <barkdawg11@Gmail.com>
2023-09-11 17:39:08 +0000

    yes

--- flask_import_server.py
+++ flask_import_server.py
@@ -1,10 +1,34 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, session
+import os
+
+
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def recallLastOrder():
+    file_path = '/home/ftp/WWtrackingImport/lastorder'
+
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as f:
+            f.write('')
+            logger.info(f"created new file: {file_path}")
+
+    try:
+        with open(file_path, 'r') as f:
+            content = f.read()
+            logger.info(f"reading last order datetime from file: {content}")
+        return content
+    except:
+        return 0
+
+@app.route('/', methods=('GET', 'POST'))
+def import_page():
+	if request.method == 'POST':
+		cutoff_date = request.form['date']
+		print(cutoff_date)
+	return render_template('import.html', last_order=recallLastOrder())
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, port=3002)
